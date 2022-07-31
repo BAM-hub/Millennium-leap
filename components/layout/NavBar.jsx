ad8ced612afc5d9804ba5111f2b3d3d454210209
BAM-hub <bsharamin12@gmail.com>
2022-07-31 21:05:38 +0300

    nav changes

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -24,50 +24,8 @@ const NavBar = () => {
             <NavLinks />
           </div>
           <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-            <button
-              type='button'
-              className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
-            >
-              <span className='sr-only'>View notifications</span>
-              <svg
-                className='h-6 w-6'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='2'
-                stroke='currentColor'
-                aria-hidden='true'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
-                />
-              </svg>
-            </button>
-
-            <div className='ml-3 relative'>
-              <div>
-                <button
-                  type='button'
-                  className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
-                  id='user-menu-button'
-                  aria-expanded='false'
-                  aria-haspopup='true'
-                  onClick={() => setShowMenu(!showMenu)}
-                >
-                  <span className='sr-only'>Open user menu</span>
-                  <Image
-                    className='h-8 w-8 rounded-full'
-                    src={avatar}
-                    alt=''
-                    height={50}
-                    width={50}
-                  />
-                </button>
-              </div>
-              <DropDown showMenu={showMenu} />
-            </div>
+            {/* <User showMenu={showMenu} /> */}
+            <NoUser />
           </div>
         </div>
       </div>
@@ -82,13 +40,14 @@ const NavBar = () => {
 const NavLinks = () => (
   <div className='hidden sm:block sm:ml-6'>
     <div className='flex space-x-4'>
-      <a
-        href='#'
-        className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
-        aria-current='page'
-      >
-        Dashboard
-      </a>
+      <Link href='/'>
+        <a
+          className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+          aria-current='page'
+        >
+          Dashboard
+        </a>
+      </Link>
 
       <a
         href='#'
@@ -103,16 +62,107 @@ const NavLinks = () => (
       >
         Projects
       </a>
-
-      <a
-        href='#'
-        className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-      >
-        Calendar
-      </a>
     </div>
   </div>
 );
+
+const NoUser = () => {
+  return (
+    <>
+      <div className='hover:bg-gray-600 px-2 py-1 rounded-md'>
+        <Link href='/auth/Login'>
+          <a className='flex gap-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
+              />
+            </svg>
+          </a>
+        </Link>
+      </div>
+      <div className='hover:bg-gray-600 px-2 py-1 rounded-md'>
+        <Link href='/auth/Register'>
+          <a className='flex gap-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+              />
+            </svg>
+          </a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+const User = ({ showMenu }) => {
+  return (
+    <>
+      <button
+        type='button'
+        className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+      >
+        <span className='sr-only'>View notifications</span>
+        <svg
+          className='h-6 w-6'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth='2'
+          stroke='currentColor'
+          aria-hidden='true'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
+          />
+        </svg>
+      </button>
+
+      <div className='ml-3 relative'>
+        <div>
+          <button
+            type='button'
+            className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+            id='user-menu-button'
+            aria-expanded='false'
+            aria-haspopup='true'
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <span className='sr-only'>Open user menu</span>
+            <Image
+              className='h-8 w-8 rounded-full'
+              src={avatar}
+              alt=''
+              height={50}
+              width={50}
+            />
+          </button>
+        </div>
+        <DropDown showMenu={showMenu} />
+      </div>
+    </>
+  );
+};
 
 const MobileMenuButton = () => (
   <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
@@ -182,13 +232,6 @@ const MobileMenu = () => (
     >
       Projects
     </a>
-
-    <a
-      href='#'
-      className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-    >
-      Calendar
-    </a>
   </div>
 );
 
@@ -206,8 +249,8 @@ const DropDown = ({ showMenu }) => (
       <a
         className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 z-2'
         role='menuitem'
-        tabIndex='-1'
         id='user-menu-item-0'
+        tabIndex='-1'
       >
         Your Profile
       </a>
