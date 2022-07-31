fb17d0f1e7e275d0976e889f27b82ea0c8e2d7f0
BAM-hub <bsharamin12@gmail.com>
2022-07-31 21:03:46 +0300

    added navbar to all pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Description from "../components/dashboard/Description";
 import Hero from "../components/dashboard/Hero";
-import NavBar from "../components/layout/NavBar";
 
 export default function Home() {
   return (
     <div className='w-90 overflow-hidden'>
-      <NavBar />
       <Hero />
       <Description />
     </div>
