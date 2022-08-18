4dcd293aec37f84ec5254dd3e3122d09642a3379
BAM-hub <bsharamin12@gmail.com>
2022-08-19 00:10:49 +0300

    composed in one component

--- components/auth/Input.jsx
+++ components/auth/Input.jsx
@@ -1,22 +1,24 @@
-const PasswordInput = ({ rounded }) => {
+const Input = ({ rounded, type, placeholder, value, setValue }) => {
   return (
     <div>
-      <label htmlFor='password' className='sr-only'>
-        Password
+      <label htmlFor={placeholder} className='sr-only'>
+        {placeholder}
       </label>
       <input
-        id='password'
-        name='password'
-        type='password'
+        id={placeholder}
+        name={placeholder}
+        type={type}
         autoComplete='current-password'
         required
         className={`bg-gray-300 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ${
           rounded && "rounded-b-md"
-        }`}
-        placeholder='Password'
+        } ${placeholder === "name" && "rounded-t-md"}`}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </div>
   );
 };
 
-export default PasswordInput;
+export default Input;
